aaed2dca24f0239191877e640c0610d690ad7dd7
ronald (MacBookie Pro) <ronald@orangewise.com>
2015-07-11 21:43:26 +0200

    Do not kick of refresh when one is in progress...

--- rates.js
+++ rates.js
@@ -19,9 +19,11 @@ Rates = {
 
   baseCurrency: 'EUR',
 
+  refreshing: false,
+
   conversionRate: function (from, to) {
     var self = this;
-    var rate;
+    var doc, rate;
 
     try {
 
@@ -29,21 +31,29 @@ Rates = {
         rate = 1;
       }
       else if (from === self.baseCurrency && from !== to) {
-        rate = self._collection.findOne({currency: to}).rate;
+        doc = self._collection.findOne({currency: to});
+        rate = doc.rate;
       }
       else if (from !== self.baseCurrency && to === self.baseCurrency) {
-        rate = self._collection.findOne({currency: from}).rate;
+        doc = self._collection.findOne({currency: from});
+        rate = doc.rate;
         rate = 1/rate;
       }
       else if (from !== self.baseCurrency && to !== self.baseCurrency) {
         var eurRate = self._collection.findOne({currency: to}).rate;
-        rate = self._collection.findOne({currency: from}).rate;
+        doc = self._collection.findOne({currency: from});
+        rate = doc.rate;
         rate = 1/rate * eurRate;
       }
-
+      
+      if (doc && self.hoursBetween(doc.updated, new Date()) >= 0) {
+        console.log('+++++++++++++refresh');
+        self.refreshRates();
+      }
       return self.roundValues(rate, self.settings.accuracy);
     } catch (error) {
       console.log('catc',error);
+      self.refreshRates();
       return undefined;
     }
 
@@ -54,17 +64,17 @@ Rates = {
     var exchangedValue = {};
 
     var exchangeRate = self.conversionRate(from, to);
-    if (exchangeRate == undefined) {
+    if (exchangeRate === undefined) {
 
       callback && callback('Exchange rate not found in collection...');
 
     } else {
 
       var exchangedAmount = self.roundValues(amount * exchangeRate, self.settings.accuracy);
-      exchangedValue.amount = amount;
       exchangedValue.from = from;
       exchangedValue.to = to;
       exchangedValue.rate = exchangeRate;
+      exchangedValue.amount = amount;
       exchangedValue.exchangedAmount = exchangedAmount;
 
       callback && callback(null, exchangedValue);
@@ -72,6 +82,12 @@ Rates = {
 
   },
 
+  hoursBetween: function (date1, date2) {
+    var ms = date2 - date1;
+    var hours = ms * (1/3600000);
+    console.log('ms', ms, 'hours', hours);
+    return hours;
+  },
 
   roundValues: function (value, places) {
     var multiplier = Math.pow(10, places);
@@ -83,37 +99,38 @@ Rates = {
     return (fixedXML.replace(/xmlns(:\w+)?="[^"]*"/g,'')).trim();
   },
 
-  parseXML: function(xml, callback) {
+  parseXML: function(doc, callback) {
     var self = this;
-    var cleanXML = self.removeNamespaces(xml);
+    var lastModified = Date.parse(doc.headers['last-modified']);
+    var cleanXML = self.removeNamespaces(doc.content);
     var parser = new xml2js.Parser();
 
     parser.parseString(cleanXML, function(error, result){
       var currencies = result.Envelope.Cube[0].Cube[0].Cube;
-      self.upsertCurrencies(currencies);
+      self.upsertCurrencies(currencies, lastModified);
       callback && callback();
     });
 
   },
 
-  upsertCurrencies: function(currencies) {
+  upsertCurrencies: function (currencies, lastModified) {
     var self = this;
+    // Use last-modified header for updated date.
+    var updated = new Date();
+    var lastModifiedDate = new Date();
+    lastModifiedDate.setTime(lastModified);     
     _.each(currencies, function (item) {
        var currency = eval('item.$').currency;
        var rate = Number(eval('item.$').rate);
        self._collection.update(
           {currency: currency},
-          {currency: currency, rate: rate}, 
+          {currency: currency, rate: rate, updated: updated, lastModified: lastModifiedDate}, 
           {upsert: true}
        );
     });
-    self._collection.update(
-      {currency: self.baseCurrency},
-      {currency: self.baseCurrency, rate: 1}, 
-      {upsert: true}
-    );
   }
 };
+
 
 Meteor.startup( function () {
   var options = Rates.options;
@@ -121,24 +138,27 @@ Meteor.startup( function () {
   // Collection holding ECB rates.
   Rates._collection = new Mongo.Collection(options.collectionName);
   Rates._collection._ensureIndex({currency: 1}, {unique: true});
-
 });
 
 
 Rates.refreshRates =  function () {
   var self = this;
-  HTTP.get(self.settings.url, function(error, response) {
+  if (!this.refreshing) {
+    self.refreshing = true;
 
-    if (!error && response.statusCode == 200) {
-      self.parseXML(response.content, function () {
-        console.log('Rates refreshed');
-      });
-    } else {
-      console.log('Error when calling', self.settings.url, error);
-    }
+    HTTP.get(self.settings.url, function(error, response) {
+      if (!error && response.statusCode == 200) {
+        self.parseXML(response, function () {
+          self.refreshing = false;
+          console.log('Rates refreshed');
+        });
+      } else {
+        console.log('Error when calling', self.settings.url, error);
+      }
+    });    
 
-  });
-
-
+  } else {
+    console.log('Rates are currently being refreshed...');
+  }
 };
 
