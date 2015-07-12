783ad7403aa07b79928aeeee32cf010cbcc48387
ronald (MacBookie Pro) <ronald@orangewise.com>
2015-07-12 19:31:43 +0200

    bumped version number

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'robodo:exchange-rates',
   summary: 'A simple package fetching ECB exchange rates and storing them in a collection.',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/orangewise/robodo-exchange-rates.git'
 });
 
